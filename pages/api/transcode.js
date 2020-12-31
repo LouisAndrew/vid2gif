227a924f25bc259d699c13c1944504e042aaec4a
Louis Andrew <louisandrew3@gmail.com>
2020-12-31 13:43:43 +0100

    importing @ffmpeg/core to transcode.js

--- pages/api/transcode.js
+++ pages/api/transcode.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
+require('@ffmpeg/core');
 
 const ffmpeg = createFFmpeg();
 
