f94cfc7e6cc8adebe1e0208a97c7bec5f32635e2
Louis Andrew <louisandrew3@gmail.com>
2021-01-02 11:46:14 +0100

    Adding favicon.

--- pages/index.js
+++ pages/index.js
@@ -2,82 +2,55 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import App from '../src/app';
 
-const Home = () => <App />;
+const Home = () => (
+    <div>
+        <Head>
+            <meta charset="utf-8" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <meta name="theme-color" content="#000000" />
+            <meta name="description" content="Video to GIF converter" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800&display=swap"
+                rel="stylesheet"
+            />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/icons/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/icons/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/icons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/icons/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/icons/safari-pinned-tab.svg"
+                color="#5bbad5"
+            />
+            <link rel="shortcut icon" href="/icons/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta
+                name="msapplication-config"
+                content="/icons/browserconfig.xml"
+            />
+            <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <main>
+            <App />
+        </main>
+    </div>
+);
 export default Home;
-
-// export default function Home() {
-//     return (
-//         <div className={styles.container}>
-//             <Head>
-//                 <title>Create Next App</title>
-//                 <link rel="icon" href="/favicon.ico" />
-//             </Head>
-
-//             <main className={styles.main}>
-//                 <h1 className={styles.title}>
-//                     Welcome to <a href="https://nextjs.org">Next.js!</a>
-//                 </h1>
-
-//                 <p className={styles.description}>
-//                     Get started by editing{' '}
-//                     <code className={styles.code}>pages/index.js</code>
-//                 </p>
-
-//                 <div className={styles.grid}>
-//                     <a href="https://nextjs.org/docs" className={styles.card}>
-//                         <h3>Documentation &rarr;</h3>
-//                         <p>
-//                             Find in-depth information about Next.js features and
-//                             API.
-//                         </p>
-//                     </a>
-
-//                     <a href="https://nextjs.org/learn" className={styles.card}>
-//                         <h3>Learn &rarr;</h3>
-//                         <p>
-//                             Learn about Next.js in an interactive course with
-//                             quizzes!
-//                         </p>
-//                     </a>
-
-//                     <a
-//                         href="https://github.com/vercel/next.js/tree/master/examples"
-//                         className={styles.card}
-//                     >
-//                         <h3>Examples &rarr;</h3>
-//                         <p>
-//                             Discover and deploy boilerplate example Next.js
-//                             projects.
-//                         </p>
-//                     </a>
-
-//                     <a
-//                         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//                         className={styles.card}
-//                     >
-//                         <h3>Deploy &rarr;</h3>
-//                         <p>
-//                             Instantly deploy your Next.js site to a public URL
-//                             with Vercel.
-//                         </p>
-//                     </a>
-//                 </div>
-//             </main>
-
-//             <footer className={styles.footer}>
-//                 <a
-//                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                 >
-//                     Powered by{' '}
-//                     <img
-//                         src="/vercel.svg"
-//                         alt="Vercel Logo"
-//                         className={styles.logo}
-//                     />
-//                 </a>
-//             </footer>
-//         </div>
-//     );
-// }
